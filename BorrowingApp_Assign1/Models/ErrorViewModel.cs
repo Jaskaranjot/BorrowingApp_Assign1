5940c93b10bebd8ef0e4478afe2b41da46cec144
Jaskaranjot <jaskaranjot11@gmail.com>
2024-09-30 13:46:25 -0400

    Add project files.

---
+++ BorrowingApp_Assign1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BorrowingApp_Assign1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
